346dd1a6effb1d5c671411887445b6a78ed00b58
LeeHemingway <coherence0815@gmail.com>
2022-02-10 16:28:14 +0900

    complete example

--- s3/hello.py
+++ s3/hello.py
@@ -1,3 +1,4 @@
+import os
 import boto3
 
 ADDRESS = os.getenv('ADDRESS_PORT', 'localstack:4566')
@@ -6,20 +7,26 @@ SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', 'secret')
 
 my_session = boto3.Session(
     aws_access_key_id=ACCESS_ID, 
-    aws_secret_access_SECRET_KEY,
+    aws_secret_access_key=SECRET_KEY,
     region_name = 'us-west-2')
 
 # default session will look at `~/.aws/credentials`
 #s3 = boto3.resource(
 
 s3 = my_session.resource(
-    servdice_name = 's3',
+    service_name = 's3',
     endpoint_url=f"http://{ADDRESS}",
     region_name='us-west-2')
 
-#s3.meta.client.upload_file(
-#    Filename = 'test.txt', 
-#    Bucket = '', 
-#    Key = '')
+my_bucket = s3.create_bucket(Bucket='mybucket')
+
+s3.meta.client.upload_file(
+    Filename = 'test.txt', 
+    Bucket = 'mybucket', 
+    Key = 'test_file_on_s3.txt')
+
+for file in my_bucket.objects.all():
+    print("files in the bucket")
+    print(file.key)
 
 
